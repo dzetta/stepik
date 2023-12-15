c0b03e3c2cf5f4dd7db75c9db5a5320fdbb0bdbf
dzetta <a.i.kochergina>
2023-12-15 21:26:29 +0300

    сетевой сниппет

--- app/src/main/java/rihilke/stepik/MainActivity.kt
+++ app/src/main/java/rihilke/stepik/MainActivity.kt
@@ -21,6 +21,9 @@ import io.reactivex.rxkotlin.zipWith
 import io.reactivex.schedulers.Schedulers
 import org.jetbrains.annotations.Async.Schedule
 import rihilke.stepik.ui.theme.StepikTheme
+import java.lang.RuntimeException
+import java.net.HttpURLConnection
+import java.net.URL
 
 /*
 * Все активити отдельны. Они ничего друг о друге не знают.
@@ -126,7 +129,21 @@ class MainActivity : ComponentActivity() {
         * */
         val o = Observable.create<String>{
             //net
-            it.onNext("test")
+            //it.onNext("test")
+            /* сниппет * */
+            val urlConnection = URL(url).openConnection() as HttpURLConnection
+            try {
+                urlConnection.connect()
+                if(urlConnection.responseCode != HttpURLConnection.HTTP_OK)
+                    it.onError(RuntimeException(urlConnection.responseMessage))
+                else {
+                    val str = urlConnection.inputStream.bufferedReader().readText()
+                    it.onNext(str)
+                }
+            } finally {
+                urlConnection.disconnect()
+            }
+            /*конец сниппета*/
         }.flatMap { Observable.create<String>{} }
             .zipWith(Observable.create<String>{})
             .map{ it.second+it.first}
